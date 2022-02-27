376d6aa143c0b48ca4d0260e460f2b94bba42ea8
Karan Aggarwal <karan.25ka@gmail.com>
2022-02-27 11:56:00 +0530

    F

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 app.secret_key = 'col362project'
 
 def get_db_connection():
-    conn = psycopg2.connect(host = "localhost", database = "col362project", user = "postgres", password = "your_password")
+    conn = psycopg2.connect(host = "localhost", database = "col362project", user = "postgres", password = "")
     return conn
 
 def reset_errors():
@@ -26,24 +26,33 @@ def reset_errors():
 def before_request():
     if 'userid' not in session:
         session['userid'] = -1
+        session['restid'] = -1
         reset_errors()
         
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    if session.get('userid') > 0:
+    if session.get('userid') > 0 or session.get('restid') > 0:
         return redirect(url_for('profile'))
 
     return redirect(url_for('home'))
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    if request.method == 'POST':
+    if request.method == 'POST' and request.form['type_'] == '0':
         reset_errors()
         session['userid'] = -1
-        username=request.form['username']
-        password=request.form['password']
-
+        session['restid'] = -1
+        username=request.form['username1']
+        password=request.form['password1']
+        if len(username) == 0 or len(username.replace(' ', '')) == 0 or not username.replace(' ', '').isalnum():
+            session['error'] = 1
+            session['login_user_username_err'] = 1
+            return redirect(url_for('login'))
+        if len(password) == 0 or ' ' in password or not password.isalnum():
+            session['error'] = 1
+            session['login_user_password_err'] = 1
+            return redirect(url_for('login'))
         # Check if username is already present in the table
         conn = get_db_connection()
         cur = conn.cursor()
@@ -54,7 +63,9 @@ def login():
 
         if (len(credentials) == 0):
             # User does not exist, currently redirecting to registration
-            return redirect(url_for('register'))
+            session['error'] = 1
+            session['login_user_username_err'] = 1
+            return redirect(url_for('login'))
 
         else:
             # Check if passwords match
@@ -65,8 +76,49 @@ def login():
                 return redirect(url_for('profile'))
 
             else:
+                session['error'] = 1
+                session['login_user_password_err'] = 1
                 return redirect(url_for('login'))
+    elif request.method == 'POST' and request.form['type_'] == '1':
+        reset_errors()
+        session['restid'] = -1
+        session['userid'] = -1
+        username=request.form['username2']
+        password=request.form['password2']
+        if len(username) == 0 or len(username.replace(' ', '')) == 0 or not username.replace(' ', '').isalnum():
+            session['error'] = 1
+            session['login_rest_username_err'] = 1
+            return redirect(url_for('login'))
+        if len(password) == 0 or ' ' in password or not password.isalnum():
+            session['error'] = 1
+            session['login_rest_password_err'] = 1
+            return redirect(url_for('login'))
+        # Check if username is already present in the table
+        conn = get_db_connection()
+        cur = conn.cursor()
+        q1 = """SELECT * FROM restaurant_login WHERE username = %s"""
+        t1 = (username,)
+        cur.execute(q1,t1)
+        credentials = cur.fetchall()
 
+        if (len(credentials) == 0):
+            # User does not exist, currently redirecting to registration
+            session['error'] = 1
+            session['login_rest_username_err'] = 1
+            return redirect(url_for('login'))
+
+        else:
+            # Check if passwords match
+            hash_ = credentials[0][2].encode('utf-8')
+            if bcrypt.checkpw(password.encode('utf-8'),hash_):
+                # passwords match, login and authenticate the user
+                session['restid'] = int(credentials[0][0])
+                return redirect(url_for('restprofile'))
+
+            else:
+                session['error'] = 1
+                session['login_rest_password_err'] = 1
+                return redirect(url_for('login'))
     return render_template('login.html', sess=session)
 
 @app.route('/logout', methods=['GET', 'POST'])
@@ -82,24 +134,47 @@ def home():
 def profile():
     if session.get('userid') <= 0:
         return redirect(url_for('home'))
-    conn = get_db_connection()
-    cur = conn.cursor()
-    cur.execute("SELECT name,url FROM restaurants limit 10;")
-    restaurants = cur.fetchall()
-    return render_template('profile.html', restaurants = restaurants)
+    # conn = get_db_connection()
+    # cur = conn.cursor()
+    # cur.execute("SELECT name,url FROM restaurants limit 10;")
+    # restaurants = cur.fetchall()
+    # return render_template('profile.html', restaurants = restaurants)
+    return render_template('profile.html')
+
+@app.route('/restprofile', methods=['GET', 'POST'])
+def restprofile():
+    if session.get('restid') <= 0:
+        return redirect(url_for('home'))
+    return render_template('restprofile.html')
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
-    if request.method == 'POST':
+    if request.method == 'POST' and request.form['type_'] == '0':
         reset_errors()
         session['userid'] = -1
+        session['restid'] = -1
         username=request.form['username3']
         password=request.form['password3']
+        if len(username) == 0 or len(username.replace(' ', '')) == 0 or not username.replace(' ', '').isalnum():
+            session['error'] = 1
+            session['reg_user_username_err'] = 1
+            return redirect(url_for('register'))
+        if len(password) == 0 or ' ' in password or not password.isalnum():
+            session['error'] = 1
+            session['reg_user_password_err'] = 1
+            return redirect(url_for('register'))
         hash_ = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
         hash_ = hash_.decode('utf8')
         # Now, establish a connection to the database and put the username and password in login_info
         conn = get_db_connection()
         cur = conn.cursor()
+        q1 = """SELECT * FROM user_login WHERE username = %s"""
+        t1 = (username,)
+        cur.execute(q1,t1)
+        if len(cur.fetchall()) > 0:
+            session['error'] = 1
+            session['reg_user_username_err'] = 1
+            return redirect(url_for('register'))
         cur.execute("SELECT userid FROM user_login ORDER BY userid DESC limit 1;")
         last_id = cur.fetchall()
 
@@ -108,21 +183,56 @@ def register():
             query1 = """INSERT INTO user_login VALUES (1,%s,%s)"""
             cur.execute(query1,tuple1)
             conn.commit()
+            session['userid'] = 1
 
         else:
+            session['userid'] = int(last_id[0][0])+1
             tuple1 = (str(int(last_id[0][0])+1),username,hash_)
             query1 = """INSERT INTO user_login VALUES (%s,%s,%s)"""
             cur.execute(query1,tuple1)
             conn.commit()
-
-        # Check if insertion has actually happened in the database
-
-        cur.close()
-        conn.close()
-
-        if 1 == 1: # authenticate user and register
-            session['userid'] = 1
-            return redirect(url_for('profile'))
-        else:
+        return redirect(url_for('profile'))
+    elif request.method == 'POST' and request.form['type_'] == '0':
+        reset_errors()
+        session['userid'] = -1
+        session['restid'] = -1
+        username=request.form['username4']
+        password=request.form['password4']
+        if len(username) == 0 or len(username.replace(' ', '')) == 0 or not username.replace(' ', '').isalnum():
+            session['error'] = 1
+            session['reg_rest_username_err'] = 1
             return redirect(url_for('register'))
-    return render_template('register.html')
+        if len(password) == 0 or ' ' in password or not password.isalnum():
+            session['error'] = 1
+            session['reg_rest_password_err'] = 1
+            return redirect(url_for('register'))
+        hash_ = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+        hash_ = hash_.decode('utf8')
+        # Now, establish a connection to the database and put the username and password in login_info
+        conn = get_db_connection()
+        cur = conn.cursor()
+        q1 = """SELECT * FROM restaurant_login WHERE username = %s"""
+        t1 = (username,)
+        cur.execute(q1,t1)
+        if len(cur.fetchall()) > 0:
+            session['error'] = 1
+            session['reg_rest_username_err'] = 1
+            return redirect(url_for('register'))
+        cur.execute("SELECT restaurantid FROM restaurant_login ORDER BY userid DESC limit 1;")
+        last_id = cur.fetchall()
+
+        if (len(last_id) == 0): # This is the first user
+            tuple1 = (username,hash_)
+            query1 = """INSERT INTO restaurant_login VALUES (1,%s,%s)"""
+            cur.execute(query1,tuple1)
+            conn.commit()
+            session['restid'] = 1
+
+        else:
+            session['restid'] = int(last_id[0][0])+1
+            tuple1 = (str(int(last_id[0][0])+1),username,hash_)
+            query1 = """INSERT INTO restaurant_login VALUES (%s,%s,%s)"""
+            cur.execute(query1,tuple1)
+            conn.commit()
+        return redirect(url_for('restprofile'))
+    return render_template('register.html', sess=session)
